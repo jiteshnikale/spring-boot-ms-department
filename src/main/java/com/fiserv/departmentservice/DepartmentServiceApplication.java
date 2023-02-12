bbc447ffb404e5d36e7a697df0eed6463a346465
JiteshNikale <nikale.jitesh@gmail.com>
2023-02-12 23:00:31 +0530

    service registry in eureka

--- src/main/java/com/fiserv/departmentservice/DepartmentServiceApplication.java
+++ src/main/java/com/fiserv/departmentservice/DepartmentServiceApplication.java
@@ -2,9 +2,11 @@ package com.fiserv.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DepartmentServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
